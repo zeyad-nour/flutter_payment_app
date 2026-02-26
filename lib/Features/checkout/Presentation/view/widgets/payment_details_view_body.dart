6eff3fbec6f9fce818d4b55d7c2875b3a35bbbc2
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-02-26 03:48:45 +0200

    Sperated CustomCreditCard

--- lib/Features/checkout/Presentation/view/widgets/payment_details_view_body.dart
+++ lib/Features/checkout/Presentation/view/widgets/payment_details_view_body.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: camel_case_types, avoid_types_as_parameter_names, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
-import 'package:payment_getways_app/Features/checkout/Presentation/view/widgets/customCrditeCard.dart';
+import 'package:payment_getways_app/Features/checkout/Presentation/view/widgets/customCrditCard.dart';
 import 'package:payment_getways_app/Features/checkout/Presentation/view/widgets/paymentMethodListview.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
