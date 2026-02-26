cfa40369fe396d497ce399d6c788ee92cb0e9ecd
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-02-26 03:23:35 +0200

    create CustomCreditCard

--- lib/Features/checkout/Presentation/view/widgets/payment_details_view_body.dart
+++ lib/Features/checkout/Presentation/view/widgets/payment_details_view_body.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:payment_getways_app/Features/checkout/Presentation/view/widgets/paymentMethodListview.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
@@ -8,19 +9,40 @@ class PaymentDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [paymentmethodListview()]);
+    return Column(children: [paymentmethodListview(), CustomCreditCard()]);
   }
 }
 
-
-class CustomCreditCard extends StatelessWidget {
+class CustomCreditCard extends StatefulWidget {
   const CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber = '';
+
+  String expiryDate = '';
+
+  String cardHolderName = '';
+
+  String cvvCode = '';
+
+  bool showBackView = false;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-      
+        CreditCardWidget(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          onCreditCardWidgetChange: (value) {},
+        ),
       ],
     );
   }
