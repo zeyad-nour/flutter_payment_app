396561dd06c32d68a4dcf51c0831920e3c82d235
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-03-02 02:41:52 +0200

    Implement ThankYouViewBody with layout and styling for thank you message

--- lib/Features/checkout/Presentation/view/widgets/thank_you_view_body.dart
+++ lib/Features/checkout/Presentation/view/widgets/thank_you_view_body.dart
@@ -5,6 +5,30 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.height * 0.8,
+            decoration: ShapeDecoration(
+              color: Color(0xffd9d9d9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          Positioned(
+          bottom: ,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.white,
+              child: Icon(Icons.check, color: Colors.green, size: 50),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
