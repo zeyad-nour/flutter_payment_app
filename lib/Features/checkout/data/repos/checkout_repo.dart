7e171be05ae2168a043553fecca678827fe4b1a1
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-03-09 05:37:55 +0200

    Add dartz dependency and implement CheckoutRepo and Failure classes for payment processing

---
+++ lib/Features/checkout/data/repos/checkout_repo.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment_getways_app/Features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:payment_getways_app/core/utils/errors/failures.dart';
+
+abstract class CheckoutRepo { 
+  Future<Either<Failure, void>> makePayment({required PaymentIntentInputModel paymentIntentInputModel});
+}
+
+
+
---
+++ lib/core/utils/errors/failures.dart
@@ -0,0 +1,8 @@
+abstract class Failure{ 
+  final String errMessage;
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure{
+  ServerFailure(super.errMessage);
+}
